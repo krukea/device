dc831e18d077f224fe8e2af9ab398315927f4292
krukea <kruk-ea@itechlab.net>
2023-06-22 18:07:08 +0300

    feat: finish modal interactions

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,20 @@
+const modalWindows = document.querySelectorAll(".modal");
 const openBtn = document.querySelectorAll(".modal-open");
 const closeBtn = document.querySelectorAll(".modal-close");
 
-openBtn.forEach((btn) => {
+const feedbackLink = document.querySelector(".feedback__link");
+const feedback = document.querySelector(".feedback");
+const feedbackForm = document.querySelector(".feedback__form");
+const userName = feedbackForm.querySelector("#feedback-name");
+const userEmail = feedbackForm.querySelector("#feedback-email");
+const feedbackText = feedbackForm.querySelector("#feedback-message");
+
+const mapBtn = document.querySelector(".contacts__map-link");
+const mapModal = document.querySelector(".map");
+
+let storageName;
+let storageEmail;
+/* openBtn.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     e.preventDefault();
 
@@ -12,6 +25,31 @@ openBtn.forEach((btn) => {
     const modal = document.querySelector(`.${modalClass}`);
     modal.classList.add("modal--show");
   });
+}); */
+
+let isStorageSupported = true;
+
+try {
+  storageName = localStorage.getItem("username");
+  storageEmail = localStorage.getItem("email");
+} catch (error) {
+  isStorageSupported = false;
+}
+
+feedbackLink.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  feedback.classList.add("modal--show");
+
+  userName.value = storageName;
+  userName.value = storageName;
+  userName.focus();
+
+  if (storageName && !storageEmail) {
+    userEmail.focus();
+  } else if (storageName && storageEmail) {
+    feedbackText.focus();
+  }
 });
 
 closeBtn.forEach((btn) => {
@@ -20,5 +58,38 @@ closeBtn.forEach((btn) => {
 
     const target = e.target;
     target.closest("section").classList.remove("modal--show");
+    target.closest("section").classList.remove("modal--error");
   });
+});
+
+window.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    modalWindows.forEach((modal) => {
+      if (modal.classList.contains("modal--show")) {
+        e.preventDefault();
+        modal.classList.remove("modal--error");
+        modal.classList.remove("modal--show");
+      }
+    });
+  }
+});
+
+feedbackForm.addEventListener("submit", (e) => {
+  if (!userName.value || !userEmail.value || !feedbackText.value) {
+    e.preventDefault();
+    feedback.classList.remove("modal--error");
+    feedback.offsetWidth = feedback.offsetWidth;
+    feedback.classList.add("modal--error");
+  } else {
+    if (isStorageSupported) {
+      localStorage.setItem("username", userName.value);
+      localStorage.setItem("email", userEmail.value);
+    }
+  }
+});
+
+mapBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  mapModal.classList.add("modal--show");
 });
